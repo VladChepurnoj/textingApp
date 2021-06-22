de11ddb8d1fbd20adf29460d172be9c18aeebd93
Vlad <chepurnojv@gmail.com>
2021-06-22 13:11:35 +0300

    compelete crud functionality

--- router.js
+++ router.js
@@ -1,25 +1,23 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("./controllers/userController");
-const postController = require("./controllers/postController");
+const express = require('express')
+const router = express.Router()
+const userController = require('./controllers/userController')
+const postController = require('./controllers/postController')
 
-router.get("/", userController.home);
-router.post("/register", userController.register);
-router.post("/login", userController.login);
-router.post("/logout", userController.logout);
+// user related routes
+router.get('/', userController.home)
+router.post('/register', userController.register)
+router.post('/login', userController.login)
+router.post('/logout', userController.logout)
 
-//post related routes
-router.get(
-  "/create-post",
-  userController.mustBeLoggedIn,
-  postController.viewCreateScreen
-);
-router.post(
-  "/create-post",
-  userController.mustBeLoggedIn,
-  postController.create
-);
+// profile related routes
+router.get('/profile/:username', userController.ifUserExists, userController.profilePostsScreen)
 
-router.get("/post/:id", postController.viewSingle);
+// post related routes
+router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen)
+router.post('/create-post', userController.mustBeLoggedIn, postController.create)
+router.get('/post/:id', postController.viewSingle)
+router.get('/post/:id/edit', userController.mustBeLoggedIn, postController.viewEditScreen)
+router.post('/post/:id/edit', userController.mustBeLoggedIn, postController.edit)
+router.post('/post/:id/delete', userController.mustBeLoggedIn, postController.delete)
 
-module.exports = router;
+module.exports = router
